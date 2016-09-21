98841c8a0ce243b31b3472185fff50db5d784939
Zachary Gibson <zach.gibson@buzznog.com>
2016-09-21 18:53:43 -0500

    Update index

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Yasss</h2>
         </div>
         <p className="App-intro">
-          Always be learning, ya bish.
+          Always be learning.
         </p>
       </div>
     );

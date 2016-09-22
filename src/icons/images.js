9a3fa9542371f05af6335c7842eb22980636c99b
Zachary Gibson <zach.gibson@buzznog.com>
2016-09-21 22:15:41 -0500

    Add icons

---
+++ src/icons/images.js
@@ -0,0 +1,19 @@
+import sock from './sock.svg';
+import galaxy from './galaxy.svg';
+import pizza from './pizza.svg';
+import alien from './alien.svg';
+import coffee from './coffee.svg';
+import atom from './atom.svg';
+import sauron from './sauron.svg';
+import internet from './internet.svg';
+
+export default {
+  sock: sock,
+  galaxy: galaxy,
+  pizza: pizza,
+  alien: alien,
+  coffee: coffee,
+  atom: atom,
+  sauron: sauron,
+  internet: internet,
+}
